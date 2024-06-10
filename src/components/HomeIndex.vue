438f6838856113d3b4f42b041de3c08e1525b0a9
VuTrungNghia <vunghia467@gmail.com>
2024-06-10 15:00:11 +0700

    UpdateHomeIndex

--- src/components/HomeIndex.vue
+++ src/components/HomeIndex.vue
@@ -20,11 +20,40 @@ export default {
     }
   },
   mounted() {
+       let setTitle = 0;
+       let setContent = 0;
+       let setImage = 0;
+        const titles = [
+          "Tuyển dụng hành chính nhân sự 2024",
+          "Khóa Học Hành Chính Nhân Sự Cấp Tốc"
+        ];
+        const contents = [
+         "Hiện nay tuyển dụng nhân sự năm 2024 là nhu cầu tất yếu của các nhà tuyển dụng",
+         "Đăng ký khóa học ngay"
+        ]
+        const images = [
+         "https://hanhchinhnhansu.edu.vn/wp-content/uploads/2024/06/Dot-Pha-Hanh-Chinh-Nhan-Su.jpg",
+         "https://hanhchinhnhansu.edu.vn/wp-content/uploads/2024/06/Khoa-Hoc-Hanh-Chinh-Nhan-Su-Cap-Toc.jpg"
+        ]
+      setInterval(() => {
+        const currentTitle = (setTitle + 1) % titles.length;
+        const currentContent = (setContent + 1) % contents.length;
+        const currentImage = (setImage + 1) % images.length;
+        this.title = titles[currentTitle]
+        this.content = contents[currentContent]
+        this.image = images[currentImage]
+        setTitle = currentTitle;
+        setContent = currentContent;
+        setImage = currentImage;
+      }, 3000);
+      
+      /*
       setTimeout(() => {
         this.title = "Khóa Học Hành Chính Nhân Sự Cấp Tốc",
         this.content = "Đăng ký khóa học ngay",
         this.image = "https://hanhchinhnhansu.edu.vn/wp-content/uploads/2024/06/Khoa-Hoc-Hanh-Chinh-Nhan-Su-Cap-Toc.jpg"
     },3000);
+    */
   }
 }
 </script>
