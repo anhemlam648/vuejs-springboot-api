ff6b9eaa1d5b8c24ffb28c34e9eeeaed9dc23b1a
VuTrungNghia <vunghia467@gmail.com>
2024-06-10 19:37:25 +0700

    UpdateAbout

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,10 +1,34 @@
 <template>
   <div>
     <h1>About</h1>
+    <div class="aboutpage">
+      <p class="viewtitle" style="text-align: center; frontsize:20px;" >{{ title }}</p>
+      <p>{{ content }}</p>
+      <p>Phone:{{ phone }}</p>
+      <p>Email:{{ email }}</p>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  data() {
+    return {
+      title: "Thông tin liên hệ",
+      content: "Nếu có thắc mắc vui lòng liên hệ với chúng tôi!",
+      phone: "1900-576-788",
+      email: "example@gmail.com" 
+    }
+  }
 }
 </script>
+
+<style>
+.aboutpage {
+  padding: 20px;
+  border-radius: 10px; 
+  border: 5px solid #33CCFF; 
+  background: #CCFFFF;
+}
+</style>
