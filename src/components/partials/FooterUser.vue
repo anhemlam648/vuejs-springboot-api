0c501802979ab10a16bc99e604d50a99fab82efb
VuTrungNghia <vunghia467@gmail.com>
2024-06-16 13:16:06 +0700

    UpdateFooter

--- src/components/partials/FooterUser.vue
+++ src/components/partials/FooterUser.vue
@@ -1,22 +1,44 @@
 <template>
   <footer>
-    <p>&copy; 2024 Your Company. All rights reserved.</p>
+    <div class="contacts">
+     <p>{{ contact }}</p>
+    <a href="https://www.facebook.com/profile.php?id=61559823183246">facebook</a>||
+    <a href="https://www.instagram.com/khoahochanhchinhnhansu/">instagram</a>||
+    <a href="https://www.youtube.com/channel/UCyavXXYLXpI-tXI26KTapJQ">youtube</a>||
+    </div>
+    <hr>
+    <div class="images">
+   <img :src = "image" alt ="hanhchinhnhansu" style="width:3%;">
+   </div>
+    <p>{{ title }}</p>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'FooterUser'
+  name: 'FooterUser',
+  data(){
+   return {
+      title: "Company&copy; 2024 Your Company. All rights reserved.",
+      contact: "Thông  tin liên hệ",
+      image:"https://hanhchinhnhansu.edu.vn/wp-content/uploads/2024/05/cropped-logohanhchinhnhansusaoviet-min-1.jpg"
+   }
+  }
 }
 </script>
 
 <style>
 footer {
+  height:130px;
   padding: 1em;
   background-color: #f8f8f8;
   text-align: center;
   position: fixed;
   bottom: 0;
   width: 100%;
+  z-index:1;
+}
+.images{
+  height: 30px;
 }
 </style>
